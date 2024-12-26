44078bd475a2b32feddcf4e4119406d2dca3c36d
hsg4440 <sghwang4440@gmail.com>
2024-12-26 14:08:22 +0900

    Update Analysis_nptool.C
    edited th2d range

--- Analysis_nptool.C
+++ Analysis_nptool.C
@@ -16,7 +16,7 @@ void Analysis_nptool(){
     // TH1D *loss_energy = new TH1D("loss_energy","^{20}Ne Beam Energy Loss in Gas; Energy Loss [MeV]; Count",60,1,200);
     // TF1 *fitgaus = new TF1("fitgaus","gaus");
     auto lat = new TLatex();
-    TH2D *dEdX_hist = new TH2D("Energy_Loss","dE/dx;Energy/u [MeV]; dE/dx [MeV/mm]",400,0,2000,400,0,0.0003);
+    TH2D *dEdX_hist = new TH2D("Energy_Loss","dE/dx;Energy/u [MeV]; dE/dx [MeV/mm]",400,0,10,400,0,0.5);
 
     cout << eventnum << endl;
 
@@ -31,14 +31,16 @@ void Analysis_nptool(){
 
         int interaction_Size = data->GetDataMult();
         for(int i = 0 ; i < interaction_Size-1 ; i++){
-            // cout <<data->GetDataMaterial(i) << endl;
+            
             if(data->GetDataIndex(i) == data->GetDataIndex(i+1)){
                 dE = data->GetDataEnergyDeposit(i);
                 dx = data->GetDataStepLength(i);
                 Energy = data->GetDataEnergy(i);
 
-                 //dEdX_hist->Fill(Energy,dE/dx);
-                dEdX_hist->Fill(Energy/(data->GetDataMaterial(i)/931),dE/dx);
+                // cout << dx << endl;
+
+                //  dEdX_hist->Fill(Energy,dE/dx);
+               dEdX_hist->Fill(Energy/(data->GetDataMaterial(i)/931.),dE/dx);
             
             }
         }
@@ -54,7 +56,7 @@ void Analysis_nptool(){
 
 
 
-    dEdX_hist->GetZaxis()->SetRangeUser(0,1000);
+    dEdX_hist->GetZaxis()->SetRangeUser(0.01,1000);
     dEdX_hist->Draw("colZ");
     // loss_energy->Draw();
     // loss_energy->Fit("fitgaus","Q");
